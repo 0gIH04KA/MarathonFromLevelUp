80605d1a4a899a37607ce06c51901106c7181aa0
Vitaliy Vit <vitalik.vit37@gmail.com>
2020-04-28 12:56:41 +0300

    resistance calculation

---
+++ _28_04_2020_SecondProject/_28_04_2020_SecondProject.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+void main()
+{
+    setlocale(LC_ALL, "rus"); // локализация
+
+    double r1, r2, r3;
+    cout << "Введите r1 ";
+    cin >> r1;
+
+    cout << "Введите r2 ";
+    cin >> r2;
+
+    cout << "Введите r3 ";
+    cin >> r3;
+
+    cout << "\n\n\n";
+
+    double r0 = 1 / ((1/r1) + (1/r2) + (1/r3));
+
+    cout << "r0 = " <<  r0 << endl;
+}
